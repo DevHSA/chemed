5003e3738088cebc359fdc832284de8528425a2b
DevHSA <harishsa85@gmail.com>
2020-09-10 18:59:09 +0530

    Push

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useRef,useEffect} from 'react'
 
-const Canvas = () => {
+const Canvas = (props) => {
 
+    const canvasRef = useRef(null);
+
+    useEffect(() => { 
+
+        
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        context.fillStyle = '#000000'
+        context.fillRect(0,0,context.canvas.width, context.canvas.height)
+        
+    }, []); 
 
     
     return (
         <div>
-            
+
+            <canvas ref={canvasRef} {...props}/>
+
         </div>
     )
 }
